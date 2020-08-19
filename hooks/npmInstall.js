d8f0e9bdbc4a2fde2149266736e8e6f1d142847e
Diogo Carvalho <dmrccarvalho@gmail.com>
2020-08-19 13:19:27 +0100

    Update npmInstall.js

--- hooks/npmInstall.js
+++ hooks/npmInstall.js
@@ -1,9 +1,6 @@
-var child_process = require('child_process');
-
 module.exports = function (context) {
-    var q = require('q');
-    const Q = context.q;
-	var deferral = Q.defer();
+	var child_process = require('child_process');
+	var deferral = require('q').defer();
 
 	child_process.exec('npm install', {cwd:__dirname},
 		function (error) {
